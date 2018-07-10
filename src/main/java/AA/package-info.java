76f0a0998a9b542811d7403c09bb2e812b815d6b
hualiuwuxin <451740146@qq.com>
2018-07-10 17:59:01 +0800

    3

---
+++ src/main/java/AA/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ZHANGYUKUN
+ *
+ */
+package AA;
---
+++ src/main/java/com/smt/cipher/thread/Main.java
@@ -0,0 +1,29 @@
+package com.smt.cipher.thread;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+
+	//@T
+	public static void a(String[] args) throws InterruptedException, ExecutionException {
+		
+		
+	 a1();
+	 a2();
+	 
+	 
+	}
+
+	private static void a2() {
+		//a1++;
+	}
+
+	private static void a1() {
+		//a2++;
+	}
+	
+	
+
+}
